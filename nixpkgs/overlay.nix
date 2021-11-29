9c1ef22ca381af3aa8f227f8a51fa8c068214797
David Guibert <david.guibert@gmail.com>
2021-11-29 11:29:02 +0100

    basic flake.nix support with lib.packs
    impure and sandbox options should be passed as:
    
    $ nix build .#pkgs.zlib -L --impure --option sandbox false

--- nixpkgs/overlay.nix
+++ nixpkgs/overlay.nix
@@ -2,6 +2,16 @@ self: pkgs:
 with pkgs;
 
 {
+  gnutls = gnutls.overrideAttrs (old: {
+    doCheck = false; # failure test-getaddrinfo
+  });
+  libgpg-error = libgpg-error.overrideAttrs (old: {
+    doCheck = false; # failure FAIL: t-argparse 1.42
+  });
+  p11-kit = p11-kit.overrideAttrs (old: {
+    doCheck = false; # failure ERROR: test-path - missing test plan
+  });
+
   nss_sss = callPackage sssd/nss-client.nix { };
 
   patchelf = patchelf.overrideAttrs (old: {
@@ -22,6 +32,7 @@ with pkgs;
   }).overrideAttrs (old: {
     preBuild = "touch Makefile.in"; # avoid automake
     doCheck = false; # df/total-verify broken on ceph
+                     # failure test-getaddrinfo
   });
   perl = perl.override {
     zlib = buildPackages.zlib.override { fetchurl = stdenv.fetchurlBoot; };
@@ -44,6 +55,16 @@ with pkgs;
   gtk3 = gtk3.override {
     trackerSupport = false;
   };
+
+  autogen = autogen.overrideAttrs (old: {
+    postInstall = old.postInstall + ''
+      # remove $TMPDIR/** from RPATHs
+      for f in "$bin"/bin/*; do
+        local nrp="$(patchelf --print-rpath "$f" | sed -E 's@(:|^)'$TMPDIR'[^:]*:@\1@g')"
+        patchelf --set-rpath "$nrp" "$f"
+      done
+    '';
+  });
 
   openssl_1_0_2 = openssl_1_0_2.overrideAttrs (old: {
     postPatch = old.postPatch + ''
